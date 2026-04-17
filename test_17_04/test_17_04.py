c0450d74c91ace123ae98b7c87d12cd67ed5dee3
Manuel Cernigoj <manuel.cernigoj@gmail.com>
2026-04-17 15:30:20 +0200

    finalize test_17_04

--- test_17_04/test_17_04.py
+++ test_17_04/test_17_04.py
@@ -1,53 +1,84 @@
-accounts = {}  # { username: password }
-
-def login(username, password):
-    if username in accounts and accounts[username] == password:
-        print("Login effettuato.")
-    else:
-        print("Username o password errati.")
-
-def registra(username, password):
-    if username in accounts:
-        print("Utente già esistente.")
-    else:
-        accounts[username] = password
-        print("Registrazione avvenuta.")
-
-# --- Menu ---
-while True:
-    print("""Benvenuto se hai già un account fai Login, altrimenti Registrati:
-          1. Login  2. Registrati  3.Esci""")
-    scelta = int(input("Scelta: "))
-
-    match scelta:
-        case 1:
-            (input("Username: "), input("Password: "))
-        case 2:
-            registra(input("Username: "), input("Password: "))
-        case 3:
-            break
-        case _:
-            print("Scelta non valida")
-
-# --- Menu funzione ---
-def menu_funzione():
-
-    print("""Che funzione vuoi usare:
-          1. Calcolatrice   2. Stampa dati""")
-    func = int(input("Scleta: "))
-
-    match func:
-        case 1:
-            print("Hai scelto Calcloatrice")
-        case 2:
-            print("Hai scelto Stampa dati")
-        case _:
-            print("Scelta non valida")
+# --- Database accounts ---
+accounts = {}       # { username: password }
 
 # --- Memoria ---
 numeri_inseriti = []
 operazioni = []
 risultati = []
+op_limit = 4
+
+def login():
+    """Restituisce lo username se il login ha successo, None altrimenti."""
+    username = input("Username: ")
+    password = input("Password: ")
+    if username in accounts and accounts[username] == password:
+        print(f"Benvenuto, {username}!")
+        return username
+    print("Credenziali errate!")
+    return None
+
+def registra():
+    username = input("Username: ")
+    if username in accounts:
+        print("Utente già esistente.")
+        return
+    password = input("Password: ")
+    accounts[username] = password
+    print("Registrazione avvenuta. Ora puoi effettuare il login.")
+
+# --- Menu Principale ---
+
+def menu_principale():
+    """Loop finché non si ottiene un login valido. Restituisce lo username."""
+    while True:
+        print("""Benvenuto se hai già un account fai Login, altrimenti Registrati:
+          1. Login  2. Registrati  3.Esci""")
+        scelta = int(input("Scelta: "))
+
+        match scelta:
+            case 1:
+                utente = login()
+                if utente:
+                    return utente
+            case 2:
+                registra()
+            case 3:
+                print("Arrivederci!")
+                break
+            case _:
+                print("Scelta non valida")
+
+# --- Menu funzione ---
+
+def stampa_riepilogo_sessione():
+    print("LIMITE OPERAZIONI RAGGIUNTO")
+    print(f"Hai eseguito {op_limit} operazioni. Riepilogo sessione:")
+    for op in (operazioni):
+        print(f"{op.capitalize()}")
+    print("Logout...")
+
+def menu_funzione():
+
+    op_count = 0          # contatore operazioni per questa sessione di login
+    while True:
+        print("""Che funzione vuoi usare:
+              1. Calcolatrice   2. Stampa dati""")
+        func = int(input("Scleta: "))
+
+        match func:
+            case 1:
+                print("Hai scelto Calcloatrice")
+                menu_calcolatrice()
+                op_count += 1
+                if op_count >= op_limit:
+                    stampa_riepilogo_sessione()
+                    return          # torna al menu principale → re-login
+            case 2:
+                print("Hai scelto Stampa dati")
+                menu_stampa()
+            case _:
+                print("Scelta non valida")
+
 
 # --- Menu Calcolatrice ---
 def menu_calcolatrice():
@@ -58,14 +89,19 @@ def menu_calcolatrice():
     match op:
         case 1:
             print("Hai scelto Addizione!")
+            addizione()
         case 2:
             print("Hai scelto Sottrazione!")
+            sottrazione()
         case 3:
             print("Hai scelto Moltiplicazione!")
+            moltiplicazione()
         case 4:
             print("Hai scelto Divisione!")
+            divisione()
         case 5:
             print("Hai scelto Potenza!")
+            potenza()
         case _:
             print("Scelta non valida")
 
@@ -73,14 +109,14 @@ def menu_calcolatrice():
 def menu_stampa():
 
     print("""Scegli cosa vuoi stampare:
-          1. Numeri inseriti    2. Risultati    3. Entrambi""")
+          1. Numeri inseriti    2. Risultati""")
     stamp = int(input("Scleta: "))
 
     match stamp:
         case 1:
-            print("Ecco i numeri che hai inserito:")
+            print(f"Ecco i numeri che hai inserito:\n {numeri_inseriti}")
         case 2:
-            print("Ecco i risultati delle tue operazioni:")
+            print(f"Ecco i risultati delle tue operazioni:\n{operazioni}\n{risultati}")
         case _:
             print("Scelta non valida")
 
@@ -113,10 +149,9 @@ def addizione():
                 numeri_inseriti.append(add)
             case 2:
                 risultato = sum(addendi)
-                operazioni.append("sottaddizionerazione")
+                operazioni.append("addizione")
                 risultati.append(risultato)
                 print(f"Calcolo eseguito! Il risultato è: {risultato}")
-                menu_funzione()
                 break            
             case _:
                 print("Scelta non valida")
@@ -150,7 +185,6 @@ def sottrazione():
                 operazioni.append("sottrazione")
                 risultati.append(risultato1)
                 print(f"Calcolo eseguito! Il risultato è: {risultato1}")
-                menu_funzione()
                 break            
             case _:
                 print("Scelta non valida")
@@ -168,7 +202,6 @@ def moltiplicazione():
     operazioni.append("moltiplicazione")
     risultati.append(risultato2)
     print(f"Calcolo eseguito! Il risultato è: {risultato2}")
-    menu_funzione()
 
 ## --- Divisione ---
 def divisione():
@@ -186,7 +219,6 @@ def divisione():
         print(f"Calcolo eseguito! Il risultato è: {risultato3}")
     else:
         print(f"Calcolo non eseguito! non puoi dividere per {div2}")
-    menu_funzione()
 
 ## --- Potenza ---
 def potenza():
@@ -196,11 +228,12 @@ def potenza():
     numeri_inseriti.append(pot1)
     print("Digita il secondo numero: ")
     pot2 = int(input("Secondo numero: "))
-    numeri_inseriti.append(pot1)
+    numeri_inseriti.append(pot2)
     risultato4 = pot1**pot2
     operazioni.append("potenza")
     risultati.append(risultato4)
     print(f"Calcolo eseguito! Il risultato è: {risultato4}")
-    menu_funzione()
 
-# --- 
+utente = menu_principale()
+if utente:
+    menu_funzione()
