ba51620ebec7433a0fedccbc70a5f37817ecdaa9
Manuel Cernigoj <manuel.cernigoj@gmail.com>
2026-05-04 18:00:05 +0200

    add 260504_lezione_15_continuato_es_2

--- 260504_lezione_15/03_gestione_studenti/gestione_studenti.py
+++ 260504_lezione_15/03_gestione_studenti/gestione_studenti.py
@@ -13,18 +13,56 @@ class Utente:
     def pwd(self):
         return self.__pwd
     
+    # setters
+    def set_nome(self, nuovo_nome):
+        self.__nome = nuovo_nome
+    
+    def set_pwd(self, nuova_pwd):
+        self.__pwd = nuova_pwd
+    
     def registrazione(self):
         try:
-            with open("credenziali.csv", "r+") as f:
-                credenziali = f.read()
-
+            with open("credenziali.csv", "r") as f:
+                credenziali = []
+                for riga in f:
+                    n, p = riga.strip().split(",")
+                    credenziali.append((n, p))
 
         except FileNotFoundError:
-            print("File non trovato")
+            open("credenziali.csv", "w").close()
+            credenziali = []
+        
+        self.set_nome(input("Inserire il nome utente: "))
+        nomi = [n for n, p in credenziali]
+        if self.nome in nomi:
+            print("Utente già presente, effettuare il Login")
+        else:
+            self.set_pwd(input("Inserire la password: "))
+            with open("credenziali.csv", "a") as f:
+                f.write(f"{self.nome},{self.pwd}\n")
 
     def login(self):
-        pass
+        try:
+            with open("credenziali.csv", "r") as f:
+                credenziali = []
+                for riga in f:
+                    n, p = riga.strip().split(",")
+                    credenziali.append((n, p))
 
+        except FileNotFoundError:
+            print("Nessun utente registrato, effettuare la registrazione")
+            return False
+    
+        self.set_nome(input("Inserire il nome utente: "))
+        self.set_pwd(input("Inserire la password: "))
+    
+        if (self.nome, self.pwd) in credenziali:
+            print(f"Benvenuto {self.nome}!")
+            return True
+        else:
+            print("Credenziali errate")
+            return False
+    
     def ins_mod(self):
         pass
 
